02bc6539c66ce9a9fa2a9b423e6d249ca4575f36
ptrhaney <84342049+ptrhaney@users.noreply.github.com>
2024-05-16 08:07:48 -0600

    Update main.js
    update .csv file

--- main.js
+++ main.js
@@ -107,7 +107,7 @@ require([
 
   // define the earthquakes layer
   const earthquakeLayer = new CSVLayer({
-    url: "./earthquake_data_complete1973.csv",
+    url: "./south of new zealand to triple junction.csv",
     elevationInfo: exaggeratedElevation,
     screenSizePerspectiveEnabled: false,
     renderer: {
